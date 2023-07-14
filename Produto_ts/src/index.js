55719dfaaf5e093f181c8f091131808e8b01fae3
Varoni <varonidiogo@gmail.com>
2023-07-13 22:30:55 -0300

    Implemmentação rota POST e arquivo teste

--- Produto_ts/src/index.js
+++ Produto_ts/src/index.js
@@ -11,6 +11,13 @@ app.get("/", (request, response) => {
     });
 });
 
+//Método POST
+app.post("/", (request, response) => {
+    return response.status(201).json({
+        message : "Produto cadastrado."
+    });
+})
+
 app.listen(3000, () => {
     console.clear();    //Sempre limpa o console antes de mostrar a aplicação rodando
     console.log("Aplicação rodando na porta 3000");
