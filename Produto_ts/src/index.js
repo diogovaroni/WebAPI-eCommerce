1f62e244d1e983c4bffce5c3f0eca1cf6c63202e
Varoni <varonidiogo@gmail.com>
2023-07-13 21:57:21 -0300

    Implementação rota GET

--- Produto_ts/src/index.js
+++ Produto_ts/src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+//Método GET
+app.get("/", (request, response) => {
+    return response.status(200).json({
+        message : "Deu certo!"
+    });
+});
+
+app.listen(3000, () => {
+    console.clear();    //Sempre limpa o console antes de mostrar a aplicação rodando
+    console.log("Aplicação rodando na porta 3000");
+});
