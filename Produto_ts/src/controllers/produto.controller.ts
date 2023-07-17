0d7821b381e4d478c54d7a4e8fa8adf1c085e481
Varoni <varonidiogo@gmail.com>
2023-07-16 23:25:40 -0300

    Implementação métodos list e create no Controller

--- Produto_ts/src/controllers/produto.controller.ts
+++ Produto_ts/src/controllers/produto.controller.ts
@@ -1,7 +1,23 @@
+import { Request, Response } from "express";
+import { Produto } from "../models/produto.model";
+
+const produtos : Produto[] = [];
 export class ProdutoController{
-    list(){}
+    list(request: Request, response: Response){
+        return response.status(200).json({
+            message: "Deu certo!",
+            data : produtos
+        })
+    }
     find(){}
-    create(){}
+    create(request: Request, response: Response){
+        const produto : Produto = request.body;
+        produtos.push(produto);
+        return response.status(201).json({
+            message : "Produto cadastrado.",
+            data : produto
+        });
+    }
     delete(){}
     update(){}
 }
--- Produto_ts/src/index.ts
+++ Produto_ts/src/index.ts
@@ -10,24 +10,3 @@ app.listen(3000, () => {
     console.clear();    //Sempre limpa o console antes de mostrar a aplicação rodando
     console.log("Aplicação rodando na porta 3000");
 });
-
-// const produtos : Produto[] = [];
-// //Método GET
-// app.get("/", (request, response) => {
-//     return response.status(200).json({
-//         message : "Deu certo!",
-//         data : produtos
-//     });
-// });
-
-// //Método POST
-// app.post("/", (request, response) => {
-//     const produto = request.body;
-//     produtos.push(produto);
-
-//     return response.status(201).json({
-//         message : "Produto cadastrado.",
-//         data : produto
-//     });
-// });
-
