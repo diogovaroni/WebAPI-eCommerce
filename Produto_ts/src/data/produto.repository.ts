a74dae26121576615c8bb1eb71ee1fcc7864598e
Varoni <varonidiogo@gmail.com>
2023-07-18 22:00:21 -0300

    Implementação dos métodos no repository

--- Produto_ts/src/data/produto.repository.ts
+++ Produto_ts/src/data/produto.repository.ts
@@ -3,5 +3,21 @@ import { Produto } from "../models/produto.model";
 let produtos : Produto[] = [];
 
 export class ProdutoRepository{
+    list(): Produto[]{
+        return produtos;
+    }
 
+    create(produto: Produto): Produto{
+        if (produtos.length == 0){
+            produto.id = 1;
+        } else{
+            produto.id = produtos[produtos.length -1].id + 1;
+        }
+        produtos.push(produto);
+        return produto;
+    }
+
+    find(id: number) : Produto{
+        return produtos.find((p) => p.id === id)!;
+    }
 }
