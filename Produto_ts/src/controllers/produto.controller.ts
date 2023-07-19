8a95d2a9c2f132ed6e33224e7df6f576a994ec97
Varoni <varonidiogo@gmail.com>
2023-07-18 22:08:10 -0300

    Opdate Controller de produto

--- Produto_ts/src/controllers/produto.controller.ts
+++ Produto_ts/src/controllers/produto.controller.ts
@@ -1,25 +1,32 @@
 import { Request, Response } from "express";
 import { Produto } from "../models/produto.model";
+import { ProdutoRepository } from "../data/produto.repository";
 
-const produtos : Produto[] = [];
+const repository = new ProdutoRepository();
 export class ProdutoController{
     list(request: Request, response: Response){
+        const produtos = repository.list();
         return response.status(200).json({
             message: "Deu certo!",
             data : produtos
         })
     }
+
     find(request: Request, response: Response){
-        const { nome } = request.params;
-        const produto : Produto = produtos.find(p => p.nome == nome)!;
+        const id = Number.parseInt(request.params.id);
+        const produto : Produto = repository.find(id);
+        if(!produto){
+            return response.status(404).json({ message: "Produto não encontrado"});
+        }
         return response.status(200).json({
             message: "Resultado da busca: ",
             data: produto
-        })
+        });
     }
+
     create(request: Request, response: Response){
-        const produto : Produto = request.body;
-        produtos.push(produto);
+        let produto : Produto = request.body;
+        produto = repository.create(produto);
         return response.status(201).json({
             message : "Produto cadastrado.",
             data : produto
