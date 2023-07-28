74090c33b60af5f41486db1f4aef373b463a35a8
Varoni <varonidiogo@gmail.com>
2023-07-27 21:00:17 -0300

    Criando rotas de Item e arquivo de teste

--- Produto_ts/src/config/routes.ts
+++ Produto_ts/src/config/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { ProdutoController } from "../controllers/produto.controller";
+import { ItemController } from "../controllers/item.controller";
 
 const router : Router = Router();
 
@@ -9,5 +10,10 @@ router.get("/produto/:id", new ProdutoController().find);
 router.post("/produto", new ProdutoController().create);
 router.delete("/produto/:id", new ProdutoController().delete);
 router.put("/produto", new ProdutoController().update);
+
+//Carrinho
+router.get("/item", new ItemController().list);
+router.get("/item/:id", new ItemController().find);
+router.post("/item/:idProduto/:quantidade/:id?", new ItemController().create);
 
 export { router };
